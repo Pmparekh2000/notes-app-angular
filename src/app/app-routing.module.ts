22b12617cd582670d797b267ae6624d90522e8aa
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-05-01 16:37:12 +0530

    Basic componenets added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NotesListComponent}
+  // An empty string '' will load this componenet when app is started
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
