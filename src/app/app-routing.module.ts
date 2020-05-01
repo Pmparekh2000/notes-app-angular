3e8f2bacea48fbd8dad314998ce7e473808ac845
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-05-01 23:22:07 +0530

    Basic front-end made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', component: NotesListComponent}
+  {path: '', component: MainLayoutComponent, children:[
+    {path: '', component: NotesListComponent}
+  ]}
+  // Over here nesting of components is done.
   // An empty string '' will load this componenet when app is started
 ];
 
